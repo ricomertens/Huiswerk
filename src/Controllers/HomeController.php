c4c321051af9172382e243b1221a612739c92cab
ricolistiq <ricovdeems@gmail.com>
2026-02-05 11:21:01 +0100

    Revert "api in"
    This reverts commit 37890dc465afb68c0f421834b887da0f01a46e78.

--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -4,62 +4,20 @@ declare(strict_types=1);
 
 namespace App\Controllers;
 
-use GuzzleHttp\Psr7\Response as GuzzleResponse;
 use GuzzleHttp\Psr7\Utils;
+use GuzzleHttp\Psr7\Response as guzzleResponse;
+use Nyholm\Psr7\Response as nyholmResponse;
 use Psr\Http\Message\ResponseInterface;
 
 class HomeController
 {
     public function index(): ResponseInterface
     {
-        $json = file_get_contents(
-            "https://weerlive.nl/api/weerlive_api_v2.php?key=a1d5da060b&locatie=sneek"
-        );
+        $stream = Utils::streamFor("Homepage");
 
-        $data = json_decode($json, true);
-        $weer = $data['liveweer'][0] ?? [];
+        $response = new GuzzleResponse;
+        $response = $response->withBody($stream);
 
-        $plaats = $weer['plaats'] ?? 'Onbekend';
-        $temperatuur = (float)($weer['temp'] ?? 10);
-        $zicht = (int)($weer['zicht'] ?? 10);
-        $soortWeer = strtolower($weer['samenv'] ?? 'onbekend');
-
-        $alarmCode = (int)($weer['alarm'] ?? 0);
-        $alarmTekst = match ($alarmCode) {
-            1 => 'Code geel',
-            2 => 'Code oranje',
-            3 => 'Code rood',
-            default => 'Code groen',
-        };
-
-        $wegen = [
-            'A7',
-            'N354',
-            'Ring Sneek',
-            'Industrieterrein Houkesloot',
-            'Centrum Sneek'
-        ];
-        
-        $output  = "Plaats: {$plaats}\n";
-        $output .= "Temperatuur: {$temperatuur} °C\n";
-        $output .= "Zicht: {$zicht} m\n";
-        $output .= "Weer: {$soortWeer}\n";
-        $output .= "Weercode: {$alarmTekst}\n\n";
-        $output .= "Beslissing per weg:\n";
-
-        foreach ($wegen as $weg) {
-            $actie = 'Geen actie';
-
-            if (str_contains($soortWeer, 'sneeuw')) {
-                $actie = 'Sneeuwschuiver + Strooien';
-            } elseif ($temperatuur <= 1) {
-                $actie = 'Strooien';
-            }
-
-            $output .= "- {$weg}: {$actie}\n";
-        }
-
-        $stream = Utils::streamFor("<pre>{$output}</pre>");
-        return (new GuzzleResponse)->withBody($stream);
+        return $response;
     }
 }
