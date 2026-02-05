36830960835e9fd3bbe860209cf5bdaee5830289
ricolistiq <ricovdeems@gmail.com>
2026-02-05 11:19:27 +0100

    Revert "berekenen van aantal strooiwagens en verbetering met alles"
    This reverts commit 4208ef679a432cba54334b78fa5ef350025737af.

--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -12,7 +12,6 @@ class HomeController
 {
     public function index(): ResponseInterface
     {
-        // de weersomstandigheden ophalen via de api
         $json = file_get_contents(
             "https://weerlive.nl/api/weerlive_api_v2.php?key=a1d5da060b&locatie=sneek"
         );
@@ -22,91 +21,45 @@ class HomeController
 
         $plaats = $weer['plaats'] ?? 'Onbekend';
         $temperatuur = (float)($weer['temp'] ?? 10);
+        $zicht = (int)($weer['zicht'] ?? 10);
         $soortWeer = strtolower($weer['samenv'] ?? 'onbekend');
 
-        // de wegen en informatie
+        $alarmCode = (int)($weer['alarm'] ?? 0);
+        $alarmTekst = match ($alarmCode) {
+            1 => 'Code geel',
+            2 => 'Code oranje',
+            3 => 'Code rood',
+            default => 'Code groen',
+        };
+
         $wegen = [
-            [
-                'id' => 1,
-                'naam' => 'A7',
-                'locatie' => 'Snelweg',
-                'duur' => 30, // minuten per strooibeurt
-                'frequentie' => [
-                    -4 => 3,
-                    -1 => 2,
-                    1  => 1
-                ]
-            ],
-            [
-                'id' => 2,
-                'naam' => 'N354',
-                'locatie' => 'Provinciale weg',
-                'duur' => 25,
-                'frequentie' => [
-                    -4 => 3,
-                    -1 => 2,
-                    1  => 1
-                ]
-            ],
-            [
-                'id' => 3,
-                'naam' => 'Centrum Sneek',
-                'locatie' => 'Stad',
-                'duur' => 20,
-                'frequentie' => [
-                    -4 => 2,
-                    -1 => 1,
-                    1  => 1
-                ]
-            ],
+            'A7',
+            'N354',
+            'Ring Sneek',
+            'Industrieterrein Houkesloot',
+            'Centrum Sneek'
         ];
-
-        // bepaling via temperatuur
-        function bepaalFrequentie(float $temp, array $frequenties): int
-        {
-            if ($temp <= -4) {
-                return $frequenties[-4] ?? 0;
-            }
-            if ($temp <= -1) {
-                return $frequenties[-1] ?? 0;
-            }
-            if ($temp <= 1) {
-                return $frequenties[1] ?? 0;
-            }
-            return 0;
-        }
-
-        // het berekenen van het aantal strooibeurten en totale tijd
-        $totaalMinuten = 0;
+        
         $output  = "Plaats: {$plaats}\n";
         $output .= "Temperatuur: {$temperatuur} °C\n";
-        $output .= "Weer: {$soortWeer}\n\n";
-        $output .= "Berekening per weg:\n";
+        $output .= "Zicht: {$zicht} m\n";
+        $output .= "Weer: {$soortWeer}\n";
+        $output .= "Weercode: {$alarmTekst}\n\n";
+        $output .= "Beslissing per weg:\n";
 
         foreach ($wegen as $weg) {
-            $aantalKeer = bepaalFrequentie($temperatuur, $weg['frequentie']);
+            $actie = 'Geen actie';
 
-            // Als het niet vriest of sneeuwt, hoeft er niet gestrooid te worden
-            if ($aantalKeer === 0 && !str_contains($soortWeer, 'sneeuw')) {
-                $output .= "- {$weg['naam']}: Geen actie\n";
-                continue;
+            if (str_contains($soortWeer, 'sneeuw')) {
+                $actie = 'Sneeuwschuiver + Strooien';
+            } elseif ($temperatuur <= 1) {
+                $actie = 'Strooien';
             }
 
-            $minuten = $aantalKeer * $weg['duur'];
-            $totaalMinuten += $minuten;
-
-            $output .= "- {$weg['naam']}: {$aantalKeer}x strooien ({$minuten} minuten)\n";
+            $output .= "- {$weg}: {$actie}\n";
         }
 
-        // berekenen van het aantal strooiwagens
-        $minutenPerWagen = 240; // 4 uur per dag
-        $aantalWagens = (int) ceil($totaalMinuten / $minutenPerWagen);
-
-        $output .= "\nTotale strooitijd: {$totaalMinuten} minuten\n";
-        $output .= "Aantal strooiwagens nodig: {$aantalWagens}\n";
-
-        // output
         $stream = Utils::streamFor("<pre>{$output}</pre>");
-        return (new GuzzleResponse())->withBody($stream);
+        return (new GuzzleResponse)->withBody($stream);
     }
 }
