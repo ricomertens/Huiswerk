07a2d5cf9da2177b4fc6b4f31249058abef06455
ricolistiq <ricovdeems@gmail.com>
2026-02-12 11:12:30 +0100

    Environments

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -2,13 +2,12 @@
 
 declare(strict_types=1);
 
-use GuzzleHttp\Psr7\ServerRequest;
-use HttpSoft\Emitter\SapiEmitter;
-use League\Route\Router;
-use League\Route\Strategy\ApplicationStrategy;
 use Dotenv\Dotenv;
-
-ini_set("display_errors", 1);
+use League\Route\Router;
+use HttpSoft\Emitter\SapiEmitter;
+use GuzzleHttp\Psr7\ServerRequest;
+use League\Route\Strategy\ApplicationStrategy;
+use League\Route\Http\Exception\NotFoundException;
 
 define("APP_ROOT", dirname(__DIR__));
 
@@ -16,6 +15,12 @@ require APP_ROOT . "/vendor/autoload.php";
 
 $dotenv = Dotenv::createImmutable(APP_ROOT);
 $dotenv->load();
+
+$env = $_ENV["APP_ENV"] ?? "prod";
+
+require $env === "dev"
+    ? APP_ROOT . "/config/errors_dev.php"
+    : APP_ROOT . "/config/errors_prod.php";
 
 $request = ServerRequest::fromGlobals();
 
@@ -36,7 +41,25 @@ $router->setStrategy($strategy);
 $routes = require APP_ROOT . "/config/routes.php";
 $routes($router);
 
-$response = $router->dispatch($request);
+try {
+
+    $response = $router->dispatch($request);
+
+} catch (NotFoundException $e) {
+    
+    http_response_code(404);
+
+    if ($env === "dev") {
+
+        throw $e;
+
+    } else {
+
+        require APP_ROOT . "/views/404.html";
+        exit;
+
+    }
+}
 
 $emitter = new SapiEmitter;
 
